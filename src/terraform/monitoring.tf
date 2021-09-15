67957973d5249f34ec90b7c12a8e69768b72c501
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-09-15 15:29:38 +0300

    ssh key

--- src/terraform/monitoring.tf
+++ src/terraform/monitoring.tf
@@ -85,7 +85,7 @@ resource "azurerm_linux_virtual_machine" "monitoring" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("./files/id_rsa.pub")
   }
 
   os_disk {
