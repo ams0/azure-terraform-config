aa533fee507c4e2ee9c1b8db6e1ef0baf93c86ac
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-09-20 23:43:18 +0200

    comment

--- src/terraform/aks1.tf
+++ src/terraform/aks1.tf
@@ -9,6 +9,8 @@ resource "azurerm_kubernetes_cluster" "aks1" {
     name           = "default"
     node_count     = 2
     vm_size        = "Standard_B4ms"
+    #3 = aks1
+    #4 = aks2
     vnet_subnet_id = azurerm_subnet.subnets[3].id
   }
 
