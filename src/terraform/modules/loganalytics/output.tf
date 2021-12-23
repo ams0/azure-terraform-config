48c8834ae5c69975231c36e184eab692f56f1ade
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-12-23 11:46:18 +0100

    remove module

--- src/terraform/modules/loganalytics/output.tf
+++
@@ -1,7 +0,0 @@
-output "logwsprimarykey" {
-  value = azurerm_log_analytics_workspace.logws.primary_shared_key 
-}
-
-output "wsid" {
-  value = azurerm_log_analytics_workspace.logws.workspace_id  
-}
