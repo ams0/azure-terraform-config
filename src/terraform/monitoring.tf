65fa6e85ef4fda668dc3a2164255765c0923ae06
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-12-23 10:16:21 +0100

    no LA

--- src/terraform/monitoring.tf
+++ src/terraform/monitoring.tf
@@ -4,14 +4,14 @@ resource "azurerm_resource_group" "monitoring" {
 
   tags = var.tags
 }
-module "loganalytics" {
-  source  = "./modules/loganalytics"
-  rg_name = azurerm_resource_group.monitoring.name
-  ws_name = "logws"
+# module "loganalytics" {
+#   source  = "./modules/loganalytics"
+#   rg_name = azurerm_resource_group.monitoring.name
+#   ws_name = "logws"
 
-  tags = var.tags
+#   tags = var.tags
 
-}
+# }
 
 module "monitoring" {
   source = "./modules/vm"
