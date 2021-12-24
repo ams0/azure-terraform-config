aed94120c4b7118d22ef4d71708c35e170c0f121
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-12-24 01:04:32 +0100

    data

--- src/terraform/policies.tf
+++ src/terraform/policies.tf
@@ -3,7 +3,7 @@ data "azurerm_subscription" "current" {}
 resource "azurerm_subscription_policy_assignment" "azpolicy-addon-to-aks" {
   name                 = "azpolicy-to-aks"
   policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/0a15ec92-a229-4763-bb14-0ea34a568f8d"
-  subscription_id      = azurerm_subscription.current.id
+  subscription_id      = data.azurerm_subscription.current.id
   display_name         = "Azure Policy Add-on for Kubernetes service (AKS) should be installed and enabled on your clusters - TF"
 
 
