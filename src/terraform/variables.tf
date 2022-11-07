e646b889f4cde00bfeed4d28c7c284259b66e4a0
Alessandro Vozza <alessandro.vozza@microsoft.com>
2022-11-07 22:27:57 +0100

    rg_loc

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -6,3 +6,10 @@ variable "resources_rg_name" {
   description = "The name of the RG to host resources"
   default     = "resources"
 }
+
+variable "resources_rg_location" {
+  type        = string
+  description = "The location of the RG to host resources"
+  default     = "westeurope"
+
+}
